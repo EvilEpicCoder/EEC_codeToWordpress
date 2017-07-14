14b089c0b9485e8ef5fd3ca4064e32135f45cfa1
Igor Gavelyuk <1st.elixile@gmail.com>
2017-07-14 12:52:47 +0300

    Updated all files for some parts, utility fully working,enjoy!

--- format.js
+++ format.js
@@ -1,51 +1,86 @@
 var input="input";
 var output="output";
 var preview="preview";
+var button="button";
+var codeValue=[];
+var lineCount=1;
+var codePreviewValue=[];
+var codePreviewValueCheck=[];
+var boolPassed=false;
 
-function get(id){
-	return parserType=document.getElementById(id);
-}
-function updateScreen(){
-	console.log(input.value+"Here");
-	preview.innerText=input.value;
-	return true;
-}
+var keyWordsArray=["abstract","arguments","await","boolean",
+"break","byte","case","catch","char","class","const","continue",
+"debugger","default","delete","do","double","else","enum","eval",
+"export","extends","false","final","finally","float","for","function",
+"goto","if","implements","import","in","instanceof","int","interface",
+"let","long","native","new","null","package","private","protected",
+"public","return","short","static","super","switch","synchronized","this",
+"throw","throws","transient","true","try","typeof","var","void",
+"volatile","while","with","yield"];
+
+var keyWordsArrayOther=["Array","Date","eval","function",
+"hasOwnProperty","Infinity","isFinite","isNaN",
+"isPrototypeOf","length","Math","NaN",
+"name","Number","Object","prototype","String","toString","undefined","valueOf"];
+
+					
+var keyCharArray=["(",")","{","}","[","]",";","=","<",">","+","-",".","/",":"];
+var keyReplaceArray=["<span style='color:#952300;'>(</span><span style='color:#00B97A;'>","</span><span style='color:#952300;'>)</span>",
+					 "<span style='color:#0000FF;'>{</span><span style='color:#4D4D4D;'>","</span><span style='color:#0000FF;'>}</span>",
+					 "<span style='color:#FFA200;'>[</span>","<span style='color:#FFA200;'>]</span>",
+					 "<span style='color:#0000FF'>;</span>","<span style='color:#06724F;'>=</span>","<span style='color:#06724F;'><</span>","<span style='color:#06724F;'>></span>",
+					 "<span style='color:#06724F'>+</span>","<span style='color:#06724F;'>-</span>","<span style='color:#0000FF;'>.</span>",
+					 "<span style='color:#FF5500;'>/</span>","<span style='color:#FF0000;'>:</span>"];
 
 
-window.onload=function start(){
-var input=document.getElementById("input");
-input.addEventListener("keydown",start,false);
-
-var codeValue=input.value;
-codeValue=codeValue.split(" ");//Array [ "hello", "gdgdg", "friend" ]
-//codeValue=codeValue.join(",");//"hello,gdgdg,friend";
-console.log(codeValue[1][2]);
-//var i=0;
-for(var i=0; i<=codeValue.length;i++){
-	if(codeValue[i]=="function"){
-		codeValue[i]="<b style='color:blue'>function</b> "
-	}
-	if(codeValue[i]=="this"){
-		codeValue[i]="<i style='color:red'>this</i> "
-	}
-
-	for(var a=0; a<=codeValue[i][a].lenght-1;a++){
-		console.log(codeValue[i][a]);
-		switch(codeValue[i][a]){
-			case "(":codeValue[i]="<b>"+codeValue[i][a]+"</b>";
-			break;
-			case ")":codeValue[i]="<b>"+codeValue[i][a]+"</b>";
-			break;
-			case "{":codeValue[i]="<b>"+codeValue[i][a]+"</b>";
-			break;
-			case "}":ccodeValue[i]="<b>"+codeValue[i][a]+"</b>";
-			break;		
-		};
-	};
-	
-}
-codeValue=codeValue.join(" ");//"hello gdgdg friend";
-document.getElementById("output").value = codeValue;
-document.getElementById("preview").innerHTML = codeValue;
-input.addEventListener("keyup",start,false);
+window.onload=function start(event){
+button=document.getElementById("button");
+button.addEventListener("click",parse,false);
+//input.addEventListener("keyup",start,false);
 };
+function parse(){
+input=document.getElementById("input");
+output=document.getElementById("output");
+preview=document.getElementById("preview");
+console.log("Clicked");
+
+inputBuffer=input.value;
+decode(inputBuffer);
+
+
+}
+function decode(val){
+		//console.log(typeof(val));
+		//console.log(val.length);
+	codeValue.push("<div style='font-family:Monospace;font-size:12px;background:#E5E5FF;color:black;'>");//creating block with code
+	codeValue.push("<span style='font-family:Monospace;font-size:10px;color:#BFBFBF;'>1__ </span>");//zero line numerated
+	//we have string, chacking each character
+	for(var i=0;i<=val.length+1;i++){
+		for(var s=0;s<=keyCharArray.length;s++){
+				if(codeValue[i]==keyCharArray[s]){
+					codeValue[i]=keyReplaceArray[s];
+				}
+		}
+		if(val[i]==' '){
+			codeValue.push(' ');
+			//console.log("space");
+		}else if(val[i]=='\n'){
+			//codeValue.push(' </br> ');
+			lineCount++;
+			codeValue.push("</br> <span style='font-family:Monospace;font-size:10px;color:#BFBFBF;'>"+lineCount+"__</span>");
+			//console.log("newline");
+		}else{
+			codeValue.push(val[i]);
+			//console.log(codeValue);
+		}
+		
+	}
+	codeValue.push("</div>");
+	//codeValue=codeValue.join("");
+	//codeValue=codeValue.split(" ");
+	
+	
+	output.value=codeValue.join("");
+	preview.innerHTML=codeValue.join("");
+	//console.log(codeValue);
+}
